c354fa183b2d4cf57d9fd5fd4a6547d479973917
Theep08 <114114978+Theep08@users.noreply.github.com>
2024-11-04 19:14:41 -0800

    Got jQuery to work I think

---
+++ aboutMe.js
@@ -0,0 +1,16 @@
+
+$("li").on("mouseenter", function(){
+    $(this).css("color", "blue")
+})
+
+$("li").on("mouseleave", function(){
+    $(this).css("color", "black")
+})
+
+$("h3").on("mouseenter", function(){
+    $(this).css("color", "blue")
+})
+
+$("h3").on("mouseleave", function(){
+    $(this).css("color", "black")
+})
